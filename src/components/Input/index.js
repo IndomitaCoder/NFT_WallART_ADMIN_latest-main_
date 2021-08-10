8ca819fa245961c8f8f8f99434d05c5982c526dd
Moana <sabiu7108@gmail.com>
2021-08-10 21:30:58 +0900

    Refactor code to improve scalability

--- src/components/Input/index.js
+++
@@ -1,7 +0,0 @@
-import Button from "./Button";
-import SelectBox from "./SelectBox";
-import ImageUploadButton from "./ImageUploadButton";
-import CheckBox from "./CheckBox";
-
-export { Button, SelectBox, ImageUploadButton, CheckBox };
- 
